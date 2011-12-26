a17cdde197227a4fa3512038986262964b1397ee
Jesse Goodwin <jesse@goodwincentral.com>
2011-12-26 10:39:33 -0500

    tweaking dimming functionality

--- lib/siriproxy-isy99i.rb
+++ lib/siriproxy-isy99i.rb
@@ -31,12 +31,11 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
   listen_for(/close the garage door/i) {close_small_garage_door}
   listen_for (/cooling.*([0-9]{2})|cool setpoint.*([0-9]{2})|cooling setpoint.*([0-9]{2})/i) { |cooling_temp| set_cool_temp(cooling_temp) }
   listen_for (/heat.*([0-9]{2})|heat setpoint.*([0-9]{2})|heating setpoint.*([0-9]{2})/i) { |heating_temp| set_heat_temp(heating_temp) }
-  #listen_for(/test/i) { test }
 
 
-  listen_for (/turn on (.*)/i) do |query|
-    deviceName = URI.unescape(query.strip)
-    @dimmable = 0 #sets default as non-dimmable - has to be set to 1 in devices file otherwise
+  listen_for (/turn on (.*)/i) do |device|
+    deviceName = URI.unescape(device.strip)
+    @dimmable = 0 #sets default as non-dimmable - must be set to 1 in devices file otherwise
     deviceAddress = deviceCrossReference(deviceName)
     if deviceAddress != 0
       check_status = Rest.get("#{self.host}/rest/status/#{deviceAddress}", :basic_auth => @auth).inspect
@@ -48,7 +47,7 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
           response = ask "Would you like to adjust the brightness settings?"
             if (response =~ /yes|sure|yep|yeah|whatever|why not|ok|I guess/i)
               dim_percent = ask "OK. What percentage would you like me to set #{deviceName} to?"
-              dim_percent_adj = dim_percent.to_i * 2.55
+              dim_percent_adj = dim_percent.to_i * 2.55 #converts percent to 0-255 setpoint
               dim_percent_adj = dim_percent_adj.to_i
               say "I am setting #{deviceName} to #{dim_percent.to_i}%."
               Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/cmd/DON/#{dim_percent_adj}", :basic_auth => @auth)
@@ -57,7 +56,7 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
         elsif status == "Off"
           if @dimmable = 1
             dim_percent = ask "This device is dimmable.  What would you like to set the level to?"
-            dim_percent_adj = dim_percent.to_i * 2.55
+            dim_percent_adj = dim_percent.to_i * 2.55 #converts percent to 0-255 setpoint
             dim_percent_adj = dim_percent_adj.to_i
             say "I am setting #{deviceName} to #{dim_percent.to_i}%."
             Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/cmd/DON/#{dim_percent_adj}", :basic_auth => @auth)
@@ -69,7 +68,7 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
             response = ask "This device is already On, and it's set to 100%. Do you want to change the level?"
               if (response =~ /yes|sure|yep|yeah|whatever|why not|ok|I guess/i)
                 dim_percent = ask "OK. What percentage would you like me to set #{deviceName} to?"
-                dim_percent_adj = dim_percent.to_i * 2.55
+                dim_percent_adj = dim_percent.to_i * 2.55 #converts percent to 0-255 setpoint
                 dim_percent_adj = dim_percent_adj.to_i
                 say "I am setting #{deviceName} to #{dim_percent.to_i}%."
                 Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/cmd/DON/#{dim_percent_adj}", :basic_auth => @auth)
@@ -85,8 +84,8 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
     request_completed
   end
 
-  listen_for (/turn off (.*)/i) do |query|
-    deviceName = URI.unescape(query.strip)
+  listen_for (/turn off (.*)/i) do |device|
+    deviceName = URI.unescape(device.strip)
     deviceAddress = deviceCrossReference(deviceName)
       if deviceAddress != 0
         check_status = Rest.get("#{self.host}/rest/status/#{deviceAddress}", :basic_auth => @auth).inspect
@@ -107,8 +106,8 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
   end
 
 
-  listen_for (/get status of (.*)/i) do |query|
-    deviceName = URI.unescape(query.strip)
+  listen_for (/get status of (.*)/i) do |device|
+    deviceName = URI.unescape(device.strip)
     @dimmable = 0 #sets default as non-dimmable - has to be set to 1 in devices file otherwise
     deviceAddress = deviceCrossReference(deviceName)
       if deviceAddress != 0
@@ -117,14 +116,14 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
         status = status.gsub(/", "uom.*$/, "")
         status_dimmer = status.to_i
           if status_dimmer > 0
-            say "Status of #{deviceName} is On and set to #{status_dimmer}%"
+            say "Status of #{deviceName} is On, and it's set to #{status_dimmer}%"
           elsif status == "On" || "Off"
             if @dimmable = 1 && status == "On"
               say "Status of #{deviceName} is On at 100%"
             else say "Status of #{deviceName} is #{status}"
             end
           else status = "error"
-            say "I'm sorry, there seems to be an error and I am unable to return status for #{deviceName}"
+            say "I'm sorry, but there seems to be an error and I am unable to return status for #{deviceName}"
           end
       else say "I'm sorry, but I am not programmed to control #{deviceName}."
       end
@@ -132,8 +131,33 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
   end
 
 
-  listen_for (/temperature.*inside|inside.*temperature|temperature.*in here/i) do |keyword, query|
-    #deviceName = URI.unescape(query.strip)
+  listen_for (/(dim|damn|jim|jimmer|turn down|turn up|set dimmer on|set level on|set the level on) (.*)/i) do |keywords, device|
+    deviceName = URI.unescape(device.strip)
+    @dimmable = 0
+    deviceAddress = deviceCrossReference(deviceName)
+    if deviceAddress != 0
+      check_status = Rest.get("#{self.host}/rest/status/#{deviceAddress}", :basic_auth => @auth).inspect
+      status = check_status.gsub(/^.*tted"=>"/, "")
+      status = status.gsub(/", "uom.*$/, "")
+      status_dimmer = status.to_i
+        if @dimmable = 1
+          dim_percent = ask "What would you like to set the level to?"
+          dim_percent_adj = dim_percent.to_i * 2.55 #converts percent to 0-255 setpoint
+          dim_percent_adj = dim_percent_adj.to_i
+          say "I am setting #{deviceName} to #{dim_percent.to_i}%."
+          Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/cmd/DON/#{dim_percent_adj}", :basic_auth => @auth)
+        elsif @dimmable = 0
+          say "I'm sorry, but #{deviceName} is not dimmable."
+        else status = "error"
+             say "I'm sorry, but there seems to be an error and I am currently unable to control #{deviceName}"
+        end
+    else say "I'm sorry, but I am not programmed to control #{deviceName}."
+    end
+    request_completed
+  end
+
+
+  listen_for (/temperature.*inside|inside.*temperature|temperature.*in here/i) do 
     deviceName = "thermostat"
     deviceAddress = deviceCrossReference(deviceName)
       if deviceAddress != 0
@@ -147,7 +171,7 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
   end
 
 
-  listen_for (/thermostat.*status|status.*thermostat/i) do |keyword, query|
+  listen_for (/thermostat.*status|status.*thermostat/i) do 
     deviceName = "thermostat"
     deviceAddress = deviceCrossReference(deviceName)
     #say "Checking the status of the thermostat."
@@ -169,20 +193,20 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
 
 
   def set_cool_temp(cooling_temp)
-    say "One moment while I set the cooling setpoint to #{cooling_temp} degrees."
     deviceName = "thermostat"
     deviceAddress = deviceCrossReference(deviceName)
     cooling_temp = cooling_temp.to_i * 2   #necessary as thermostat input must be doubled
+    say "One moment while I set the cooling setpoint to #{cooling_temp} degrees."
     Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/set/CLISPC/#{cooling_temp}", :basic_auth => @auth).inspect
     request_completed
   end
 
 
   def set_heat_temp(heating_temp)
-    say "One moment while I set the heating setpoint to #{heating_temp} degrees."
     deviceName = "thermostat"
     deviceAddress = deviceCrossReference(deviceName)
     heating_temp = heating_temp.to_i * 2   #necessary as thermostat input must be doubled
+    say "One moment while I set the heating setpoint to #{heating_temp} degrees."
     Rest.get("#{self.host}/rest/nodes/#{deviceAddress}/set/CLISPH/#{heating_temp}", :basic_auth => @auth).inspect
     request_completed
   end
@@ -190,20 +214,11 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
 
   def anything_else
     response = ask "Is there anything else you would like me to do?"
-      if (response =~ /yes/i)
+      if (response =~ /yes|sure|yep|yeah|whatever|why not|ok|I guess/i)
         say "OK, but I'm still working on that part of my programming."
       else say "Good.  Because I can't do that yet."
       end
     request_completed
-  end
-
-
-  def test
-    Rest.get("#{self.host}/rest/nodes/1B%2041%2082%201/cmd/DON", :basic_auth => @auth)
-    say "Test complete!"
-    sleep(5)
-    Rest.get("#{self.host}/rest/nodes/1B%2041%2082%201/cmd/DOF", :basic_auth => @auth)
-    request_completed 
   end
 
 
@@ -222,7 +237,7 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
 
 
   def merry_christmas
-    response = ask "Merry Christmas! Do you want me to put the tree lights on?"
+    response = ask "Merry Christmas master! Do you want me to put the tree lights on?"
       if (response =~ /yes|sure|yep|yeah|whatever|why not|ok|I guess/i)
         Rest.get("#{self.host}/rest/nodes/24409/cmd/DON", :basic_auth => @auth)
       else say "Scrooge!"
@@ -235,18 +250,5 @@ class SiriProxy::Plugin::Isy99i < SiriProxy::Plugin
     Rest.get("#{self.host}/rest/nodes/24409/cmd/DOF", :basic_auth => @auth)
     request_completed 
   end
-
-#listen_for (/turn on/i) do
-    #response = ask "What do you want me to turn on?" 
-
-    #if(response =~ /kitchen light/i) 
-       #whatever = HTTParty.get("http://192.168.1.138/rest/nodes/1B%2041%2082%201/cmd/DON", :basic_auth => @auth)
-
-    #else
-      #say "Sorry, but I am only programmed to turn on the Kitchen Light"
-    #end
-    
-    #request_completed 
-  #end
   
 end
